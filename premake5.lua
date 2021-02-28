011fe9249c7689a54fd01793ff2665fc40f468d5
Lazergenix <lazergenixdev@gmail.com>
2021-02-28 13:41:17 -0800

    nice

--- premake5.lua
+++ premake5.lua
@@ -1,4 +1,24 @@
 workspace 'Fission'
+
+	architecture "x86_64"
+
 	configurations { 'Debug', 'Release', 'Dist' }
+
+	
+    flags { 'MultiProcessorCompile', 'MFC' }
+	
+	startproject 'Fission'
+
+	
+    OutputDir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+	
+    IncludeDir = {}
+    IncludeDir["yaml"] = "%{wks.location}/Fission/vendor/yaml-cpp/include"
+    IncludeDir["lazerlib"] = "%{wks.location}/Fission/vendor/lazerlib"
+
+group "Dependencies"
+    include "Fission/vendor/yaml-cpp"
+    include "Fission/vendor/lazerlib"
+group ""
 
 include 'Fission'
