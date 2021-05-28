9adb60ed4bb1b4e0d637158dc394c220d1e49365
bruh <lazergenixdev@gmail.com>
2021-05-27 21:39:59 -0700

    🥪 add config to base, with some helper macros

--- include/Fission/Base/Exception.h
+++ include/Fission/Base/Exception.h
@@ -28,7 +28,7 @@
 *
 */
 #pragma once
-
+#include "config.h"
 #include <exception>
 #include <filesystem>
 #include <sstream>
@@ -46,129 +46,126 @@ throw ::Fission::base::Exception( NAME, ::Fission::base::exception_message(FISSI
 #define FISSION_THROW_NOT_IMPLEMENTED() \
 FISSION_THROW( "'I'm Lazy Exception'",.append( "'" __FUNCTION__ "' not implemented." ) )
 
-namespace Fission
+_FISSION_BASE_BEGIN
+
+namespace detail
 {
-  namespace base
-  {
 
-    namespace detail
+    template <size_t _Size>
+    static constexpr size_t get_filename_size( const char (&_Filepath)[_Size] )
     {
+        size_t ret = _Size-1;
 
-      template <size_t _Size>
-      static constexpr size_t get_filename_size( const char (&_Filepath)[_Size] )
-      {
-          size_t ret = _Size-1;
+        do {
+            if( 
+                _Filepath[ret] == '\\' // Windows Filesystem
+            ||  _Filepath[ret] == '/'  // Literally every other Filesystem
+            ) return _Size - ret - 1;
+        } 
+        while( ret-- > 0u );
 
-          do {
-              if( 
-                  _Filepath[ret] == '\\' // Windows Filesystem
-               || _Filepath[ret] == '/'  // Literally every other Filesystem
-             ) return _Size - ret - 1;
-          } 
-          while( ret-- > 0u );
-
-          return _Size;
-      }
-
+        return _Size;
     }
 
+}
 
 
-    template <size_t _Filename_Size>
-    struct Filename
-    {
-        char value[_Filename_Size];
+
+template <size_t _Filename_Size>
+struct Filename
+{
+    char value[_Filename_Size];
         
-        template <size_t _Size>
-        constexpr Filename( const char (&_Path)[_Size] ) : value()
-        {
-            for( size_t i = 0; i < _Filename_Size; ++i )
-            { // Ninja Turtles
-                value[i] = _Path[i+_Size-_Filename_Size];
-            }
+    template <size_t _Size>
+    constexpr Filename( const char (&_Path)[_Size] ) : value()
+    {
+        for( size_t i = 0; i < _Filename_Size; ++i )
+        { // Ninja Turtles
+            value[i] = _Path[i+_Size-_Filename_Size];
         }
-    }; // struct Fission::base::Filename
+    }
+}; // struct Fission::base::Filename
 
 
 
-    template <typename _FilenameTy>
-    class throw_location
-    {
-    public:
-      constexpr throw_location( int line, const _FilenameTy & file ) noexcept
-        : _Line( line ), _File( file )
-      {}
+template <typename _FilenameTy>
+class throw_location
+{
+public:
+    constexpr throw_location( int line, const _FilenameTy & file ) noexcept
+    : _Line( line ), _File( file )
+    {}
 
-      constexpr int get_line() const noexcept { return _Line; }
-      constexpr _FilenameTy & get_file() const noexcept { return _File; }
+    constexpr int get_line() const noexcept { return _Line; }
+    constexpr _FilenameTy & get_file() const noexcept { return _File; }
 
-      std::string as_string() const noexcept {
-        std::stringstream ss;
-        ss << "File: " << _File.value << " @Line " << _Line;
-        return ss.str();
-      }
-    private:
-      int _Line;
-      _FilenameTy _File;
-    }; // class Fission::base::throw_location
-
-
-
-    template <typename _ThrowLocationTy>
-    class exception_message
-    {
-    public:
-      exception_message( const _ThrowLocationTy & loc ) noexcept : _msg( loc.as_string() ) {}
-
-      exception_message & append( const std::string & msg ) noexcept {
-        _msg += "\n\n";
-        _msg += msg;
-        return *this;
-      }
-
-      exception_message & append( const std::string & key, const std::string & msg ) noexcept {
-        _msg += "\n\n";
-        _msg += key;
-        _msg += ":\n";
-        _msg += msg;
-        return *this;
-      }
-
-      operator const std::string & ( ) const noexcept {
-        return _msg;
-      }
-
-    private:
-      std::string _msg;
-
-    }; // class Fission::base::exception_message
+    std::string as_string() const noexcept {
+    std::stringstream ss;
+    ss << "File: " << _File.value << " @Line " << _Line;
+    return ss.str();
+    }
+private:
+    int _Line;
+    _FilenameTy _File;
+}; // class Fission::base::throw_location
 
 
 
-    class base_exception : public std::runtime_error
-    {
-    public:
-        base_exception( const std::string & name, const std::string & msg ) noexcept
-            : std::runtime_error( msg ), _name( name )
-        {}
+template <typename _ThrowLocationTy>
+class exception_message
+{
+public:
+    exception_message( const _ThrowLocationTy & loc ) noexcept : _msg( loc.as_string() ) {}
 
-        const char * name() const noexcept { return _name.c_str(); }
+    exception_message & append( const std::string & msg ) noexcept {
+    _msg += "\n\n";
+    _msg += msg;
+    return *this;
+    }
 
-    private:
-        std::string _name;
-    }; // class Fission::base::base_exception
+    exception_message & append( const std::string & key, const std::string & msg ) noexcept {
+    _msg += "\n\n";
+    _msg += key;
+    _msg += ":\n";
+    _msg += msg;
+    return *this;
+    }
+
+    operator const std::string & ( ) const noexcept {
+    return _msg;
+    }
+
+private:
+    std::string _msg;
+
+}; // class Fission::base::exception_message
 
 
 
-    template <typename _MessageTy>
-    class Exception : public base_exception
-    {
-    public:
-      Exception( const std::string & name, const _MessageTy & message ) noexcept
+class base_exception : public std::runtime_error
+{
+public:
+    const char * name() const noexcept { return _name.c_str(); }
+
+protected:
+    base_exception( const std::string & name, const std::string & msg ) noexcept
+        : std::runtime_error( msg ), _name( name )
+    {}
+
+private:
+    std::string _name;
+}; // class Fission::base::base_exception
+
+
+
+template <typename _MessageTy>
+class Exception : public base_exception
+{
+public:
+    Exception( const std::string & name, const _MessageTy & message ) noexcept
         : base_exception( name, message )
-      {}
-    }; // class Fission::base::Exception
+    {}
+}; // class Fission::base::Exception
 
 
-  } // namespace Fission::base
-} // namespace Fission
+_FISSION_BASE_END
---
+++ include/Fission/Base/config.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#define _FISSION_BASE_BEGIN namespace Fission{namespace base{
+#define _FISSION_BASE_END }}
+
+#define _FISSION_BASE_PUBLIC_BEGIN namespace Fission{
+#define _FISSION_BASE_PUBLIC_END }
+
+#define _FISSION_BASE_ALIASES(N) \
+using N##f = N<float>;  \
+using N##d = N<double>;  \
+using N##l = N<long long>;\
+using N##i = N<int>
+
+_FISSION_BASE_BEGIN
+
+using size_t =:: size_t; // lol
+
+_FISSION_BASE_END
