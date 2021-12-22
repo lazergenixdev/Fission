657cdc854e27673f57e45a8f3cae050f2ac322b6
Lazergenix <lazergenixdev@gmail.com>
2021-12-21 21:55:24 -0800

    🥏 Headers. bro

--- demos/premake5.lua
+++ demos/premake5.lua
@@ -8,7 +8,7 @@ function include_demo(name)
   targetdir ('%{wks.location}/bin/' .. OutputDir)
   objdir ('%{wks.location}/bin-int/' .. OutputDir .. '/%{prj.name}')
 
-  files { '%{prj.location}/src/**.cpp' }
+  files { '%{prj.location}/src/**.cpp', '%{prj.location}/src/**.h' }
   links { 'Fission', 'freetype' }
 
   libdirs
