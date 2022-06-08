75b66c9cef101b7e28835b3ad1bc2c519a1f490d
lazergenixdev <lazergenixdev@gmail.com>
2022-06-08 01:02:21 -0700

    ❌ remove redundant code in premake file

--- premake5.lua
+++ premake5.lua
@@ -1,34 +1,27 @@
 IncludeDir = {}
-IncludeDir["yaml"] = "%{wks.location}/Fission/vendor/yaml-cpp/include"
-IncludeDir["json"] = "%{wks.location}/Fission/vendor/json/single_include"
-IncludeDir["freetype"] = '%{wks.location}/Fission/vendor/freetype/include'
+IncludeDir["yaml"] 		= "vendor/yaml-cpp/include"
+IncludeDir["json"] 		= "vendor/json/single_include"
+IncludeDir["freetype"] 	= 'vendor/freetype/include'
+
+group "Dependencies"
+	include "Fission/vendor/yaml-cpp"
+group ""
 
 if _FISSION_EXTERNAL then
-	IncludeDir["yaml"] =     'vendor/yaml-cpp/include'
-	IncludeDir["json"] =     'vendor/json/single_include'
-	IncludeDir["freetype"] = 'vendor/freetype/include'
 	include 'Fission'
-	group "Dependencies"
-		include "Fission/vendor/yaml-cpp"
-	group ""
 else
 	workspace 'Fission'
-		architecture "x86_64"
-		configurations { 'Debug', 'Release', 'Dist' }
-		
-		OutputDir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
-
-		flags { 'MultiProcessorCompile', 'MFC' }
-		defines '_CRT_SECURE_NO_WARNINGS'
-		
-		startproject 'sandbox'
-
-	group "Dependencies"
-		include "Fission/vendor/yaml-cpp"
-	group ""
-
+	architecture "x86_64"
+	configurations { 'Debug', 'Release', 'Dist' }
+	
+	OutputDir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+	
+	flags { 'MultiProcessorCompile', 'MFC' }
+	defines '_CRT_SECURE_NO_WARNINGS'
+	
+	startproject 'sandbox'
+	
 	include 'Fission'
 	include 'sandbox'
-
 	include 'demos'
 end
